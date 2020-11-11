1090d801d3e3e6467a700625ff8b8d38417de8cc
Andrey Tararaksin <atararaksin@gmail.com>
2020-11-11 19:17:44 +0300

    Backend

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -2,32 +2,30 @@ import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateway from '@aws-cdk/aws-apigateway';
-import * as s3Notifications from '@aws-cdk/aws-s3-notifications';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import { TodoBackend } from './todo-backend';
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new lambda.Function(this, "HelloLambda", {
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler',
-      runtime: lambda.Runtime.NODEJS_12_X,
-      memorySize: 128,
-      timeout: cdk.Duration.seconds(5),
-      environment: {isProduction: 'absolutely not'}
-    });
+    const todoBackend = new TodoBackend(this, "TodoBackend");
 
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
-      handler: helloLambda
+      handler: todoBackend.handler
     });
 
     const logoBucket = new s3.Bucket(this, "LogoBucket", {
       publicReadAccess: true
     });
 
-    logoBucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED,
-      new s3Notifications.LambdaDestination(helloLambda)
-    );
+    new s3Deployment.BucketDeployment(this, "DeployLogo", {
+      destinationBucket: logoBucket,
+      sources: [s3Deployment.Source.asset("./assets")]
+    });
+
+    new cdk.CfnOutput(this, "LogoPath", {
+      value: `https://${logoBucket.bucketDomainName}/5-6.jpg`
+    });
   }
 }
